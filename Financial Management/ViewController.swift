146d0f25cc0f44ef5c8a50fda6a887d1ef27db51
HornSeller <d0888864666@gmail.com>
2023-11-20 21:30:25 +0700

    Initial Commit

---
+++ Financial Management/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Financial Management
+//
+//  Created by Mac on 20/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
